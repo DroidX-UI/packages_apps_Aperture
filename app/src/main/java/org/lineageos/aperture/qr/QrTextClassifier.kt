LineageOS/android_packages_apps_Aperture

Aperture: Handle QR upi:// scheme
Change-Id: Ia635841e92cba4b2de5f0dcf54245922b079293a

app/src/main/java/org/lineageos/aperture/qr/QrTextClassifier.kt
@@ -83,6 +83,26 @@ class QrTextClassifier(
                         }
                     }
                     .build()
+
+                SCHEME_UPI -> return TextClassification.Builder()
+                    .setText(context.getString(R.string.qr_upi_content_description))
+                    .setEntityType(TextClassifier.TYPE_OTHER, 1.0f)
+                    .apply {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                            addAction(
+                                RemoteAction::class.build(
+                                    context,
+                                    R.drawable.ic_payments,
+                                    R.string.qr_upi_title,
+                                    R.string.qr_upi_content_description,
+                                    Intent(Intent.ACTION_VIEW).apply {
+                                        data = uri
+                                    }
+                                )
+                            )
+                        }
+                    }
+                    .build()
             }
         }
 
@@ -96,5 +116,6 @@ class QrTextClassifier(
     companion object {
         private const val SCHEME_DPP = "dpp"
         private const val SCHEME_FIDO = "fido"
+        private const val SCHEME_UPI = "upi"
     }
 }

DroidX-UI/packages_apps_Aperture

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

app/src/main/java/org/lineageos/aperture/qr/QrTextClassifier.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2023-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -83,6 +83,26 @@ class QrTextClassifier(
                         }
                     }
                     .build()
+
+                SCHEME_UPI -> return TextClassification.Builder()
+                    .setText(context.getString(R.string.qr_upi_content_description))
+                    .setEntityType(TextClassifier.TYPE_OTHER, 1.0f)
+                    .apply {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                            addAction(
+                                RemoteAction::class.build(
+                                    context,
+                                    R.drawable.ic_currency_rupee,
+                                    R.string.qr_upi_title,
+                                    R.string.qr_upi_content_description,
+                                    Intent(Intent.ACTION_VIEW).apply {
+                                        data = uri
+                                    }
+                                )
+                            )
+                        }
+                    }
+                    .build()
             }
         }
 
@@ -96,5 +116,6 @@ class QrTextClassifier(
     companion object {
         private const val SCHEME_DPP = "dpp"
         private const val SCHEME_FIDO = "fido"
+        private const val SCHEME_UPI = "upi"
     }
 }